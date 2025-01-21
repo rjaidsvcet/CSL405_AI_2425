71522dfb690197ff2a2e260c23b1c55b07d2fef8
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-01-21 15:35:49 +0530

    For Loop

---
+++ Material/Module_1/functionalScripts/2025_01_21/loops.py
@@ -0,0 +1,21 @@
+# for _ in range (10):
+#     print ('Hello')
+
+# array = [0, 1, 2, 3]
+array = list ()
+
+newElement = 10
+for _ in array:
+    array.append (newElement)
+    newElement += 10
+
+n = 10
+for _ in range (n):
+    array.append (newElement)
+    newElement += 10
+
+# for _ in range (len (array)):
+#     print (array[_])
+
+# for _ in array:
+#     print (_)
