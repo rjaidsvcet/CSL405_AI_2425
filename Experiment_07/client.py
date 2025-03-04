0fe4868ccfa074b582e36cc3638ccde5c935c34e
rjaidsvcet <raunak.joshi@vcet.edu.in>
2025-03-04 13:41:58 +0530

    Client and Server Socket

---
+++ Experiment_07/client.py
@@ -0,0 +1,26 @@
+import socket
+
+PORT = 5050
+SERVER = ''
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+HEADER = 64
+DISCONNECT_MESSAGE = '!DISCONNECT'
+
+client = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+client.connect (ADDR)
+
+def send (messages):
+    message = messages.encode (FORMAT)
+    messageLength = len (message)
+    sendLength = str (messageLength).encode (FORMAT)
+    sendLength += b' ' * (HEADER - len (sendLength))
+    client.send (sendLength)
+    client.send (message)
+    print (client.recv (2045).decode (FORMAT))
+
+if __name__ == '__main__':
+    ### Pass the Hello World message as an input
+    # ... Here
+
+    send (DISCONNECT_MESSAGE)
